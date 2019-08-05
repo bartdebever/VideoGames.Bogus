8e733245d3c078957a8e76879ca655da7cea793c
Bart de Bever <bartdebever112@gmail.com>
2019-08-05 22:43:35 +0200

    First implementation of the methods

--- Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames/IVideoGamesDataSet.cs
+++ Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames/IVideoGamesDataSet.cs
@@ -6,5 +6,42 @@ namespace Bartdebever.Bogus.VideoGames
 {
     public interface IVideoGamesDataSet
     {
+        /// <summary>
+        /// Generates a random Nintendo Switch friend code.
+        /// </summary>
+        /// <returns>
+        /// A random Nintendo Switch Friend Code.
+        /// </returns>
+        string GenerateSwitchFriendCode();
+
+        /// <summary>
+        /// Generates a random PlayStation Network Id.
+        /// </summary>
+        /// <returns>
+        /// Returns a random PlayStation Network Id.
+        /// </returns>
+        string GeneratePSNId();
+
+        /// <summary>
+        /// Generates a random XBox Id.
+        /// </summary>
+        /// <returns></returns>
+        string GenerateXBoxId();
+
+        /// <summary>
+        /// Generates a random Super Smash Bros. Ultimate Battle Arena code.
+        /// </summary>
+        /// <returns>
+        /// A random Super Smash Bros. Ultimate Battle Arena code.
+        /// </returns>
+        string GenerateUltimateArenaCode();
+
+        /// <summary>
+        /// Generates a random Super Mario Maker 2 level code using the XXX-XXX-XXX format.
+        /// </summary>
+        /// <returns>
+        /// A random Super Mario Maker 2 level code.
+        /// </returns>
+        string GenerateMarioMaker2LevelCode();
     }
 }
--- Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames/VideoGameDataSet.cs
+++ Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames/VideoGameDataSet.cs
@@ -7,5 +7,31 @@ namespace Bartdebever.Bogus.VideoGames
 {
     public class VideoGameDataSet : DataSet, IVideoGamesDataSet
     {
+        public string GenerateSwitchFriendCode()
+        {
+            const string legalCharacters = "013456789";
+
+            return $"SW-{Random.String2(4, legalCharacters)}-{Random.String2(4, legalCharacters)}-{Random.String2(4, legalCharacters)}";
+        }
+
+        public string GeneratePSNId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateXBoxId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateUltimateArenaCode()
+        {
+            return Random.AlphaNumeric(5).ToUpper();
+        }
+
+        public string GenerateMarioMaker2LevelCode()
+        {
+            return $"{Random.AlphaNumeric(3)}-{Random.AlphaNumeric(3)}-{Random.AlphaNumeric(3)}".ToUpper();
+        }
     }
 }
