c1abcf6bc608de3e96172d7be7caf0eabb33d800
Bart de Bever <bartdebever112@gmail.com>
2019-08-05 23:03:58 +0200

    Coppied documentation and simple command line based tests

---
+++ Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames.Tests/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Bogus;
+
+namespace Bartdebever.Bogus.VideoGames.Tests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var faker = new Faker();
+            var videoGames = faker.VideoGames();
+
+            Console.WriteLine("Switch FriendCodes");
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine(videoGames.GenerateSwitchFriendCode());
+            }
+
+            Console.WriteLine("Smash Arenas");
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine(videoGames.GenerateUltimateArenaCode());
+            }
+
+            Console.WriteLine("Mario Maker Levels");
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine(videoGames.GenerateMarioMaker2LevelCode());
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
--- Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames/VideoGameDataSet.cs
+++ Bartdebever.Bogus.VideoGames/Bartdebever.Bogus.VideoGames/VideoGameDataSet.cs
@@ -7,6 +7,12 @@ namespace Bartdebever.Bogus.VideoGames
 {
     public class VideoGameDataSet : DataSet, IVideoGamesDataSet
     {
+        /// <summary>
+        /// Generates a random Nintendo Switch friend code.
+        /// </summary>
+        /// <returns>
+        /// A random Nintendo Switch Friend Code.
+        /// </returns>
         public string GenerateSwitchFriendCode()
         {
             const string legalCharacters = "013456789";
@@ -14,21 +20,43 @@ namespace Bartdebever.Bogus.VideoGames
             return $"SW-{Random.String2(4, legalCharacters)}-{Random.String2(4, legalCharacters)}-{Random.String2(4, legalCharacters)}";
         }
 
+        /// <summary>
+        /// Generates a random PlayStation Network Id.
+        /// </summary>
+        /// <returns>
+        /// Returns a random PlayStation Network Id.
+        /// </returns>
         public string GeneratePSNId()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Generates a random XBox Id.
+        /// </summary>
+        /// <returns></returns>
         public string GenerateXBoxId()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Generates a random Super Smash Bros. Ultimate Battle Arena code.
+        /// </summary>
+        /// <returns>
+        /// A random Super Smash Bros. Ultimate Battle Arena code.
+        /// </returns>
         public string GenerateUltimateArenaCode()
         {
             return Random.AlphaNumeric(5).ToUpper();
         }
 
+        /// <summary>
+        /// Generates a random Super Mario Maker 2 level code using the XXX-XXX-XXX format.
+        /// </summary>
+        /// <returns>
+        /// A random Super Mario Maker 2 level code.
+        /// </returns>
         public string GenerateMarioMaker2LevelCode()
         {
             return $"{Random.AlphaNumeric(3)}-{Random.AlphaNumeric(3)}-{Random.AlphaNumeric(3)}".ToUpper();
